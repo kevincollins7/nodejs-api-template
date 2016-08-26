6137c01075ae78229d0fa9296a1fe6d83fadb901
Kevin Collins <kevin.collins@techwrights.com>
2016-08-25 21:38:55 -0700

    Added port listening and default route

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 'use strict';
-var express = require('express');
+const http = require('http');
+const express = require('express');
+
+const app = express();
+
+app.get('/', function(req, res) {
+  res.json({ "API Template" : "v1.0.0" })
+});
+
+const port = process.env.PORT || 1337;
+
+app.listen(port, function() {
+  console.log("App running on port 1337...")
+});
